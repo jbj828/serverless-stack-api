355063e7bbd5d36263fa7c9970fa550ab88f783e
Chung Byung Jae <jbj828@naver.com>
2021-01-26 17:32:11 +0900

    feat: adding serverless api

---
+++ list.js
@@ -0,0 +1,19 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    KeyConditionExpression: "userId = :userId",
+    // 'ExpressionAttributeValues' defines the value in the condition
+    ExpressionAttributeValues: {
+      ":userId": "123",
+    },
+  };
+
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
---
+++ update.js
@@ -0,0 +1,24 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      userId: "123",
+      noteId: event.pathParameters.id,
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    UpdateExpression: "SET content = :content, attachment = :attachment",
+    ExpressionAttributeValues: {
+      ":attachment": data.attachment || null,
+      ":content": data.content || null,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  await dynamoDb.update(params);
+
+  return { status: true };
+});
